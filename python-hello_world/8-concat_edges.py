25c71861d88aafa314ec939b16889ee3e93e851b
root <root@DESKTOP-46AJ964>
2025-01-15 15:41:35 +0100

    ajout du fichier 8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -2,4 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
 language that combines remarkable power with very clear syntax"
          
-print(str[39:-62] + str[105:-17] + str[0:6])
+print(str[39:62] + str[105:122] + str[0:6])

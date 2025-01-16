22f7d194e951ef3e6004d6772eb4a465b9bc1eb6
root <root@DESKTOP-46AJ964>
2025-01-16 15:04:06 +0100

    fichier 1

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -8,9 +8,9 @@ message = "Last digit of %d is %d" % (number, ld)
     
 if ld > 5:
     message += " and is greater than 5"
-elif ld == 0:
+if ld == 0:
     message += " and is 0"
-else:
+if (ld < 6) and (ld != 0):
     message += " and is less than 6 and not 0"
 
 print(message)

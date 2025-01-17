c742e4a30d0348bb2e8f5582f23ccadd71015ee3
root <root@DESKTOP-46AJ964>
2025-01-17 14:15:21 +0100

    ajout du fichier 11

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 
 def pow(a, b):
-    compute = a ^ b
+    if b == 0:
+        return 1
+    compute = 1
+    if b > 0:
+        for i in range(b):
+            compute *= a
+    if b < 0:
+        for i in range(abs(b)):
+            compute *= a
+        compute = 1 / compute
     return compute
+

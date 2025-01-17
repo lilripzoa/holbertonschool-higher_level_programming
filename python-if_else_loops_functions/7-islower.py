661916dc84d89746a78c62fe57454d6acc5ecf3e
root <root@DESKTOP-46AJ964>
2025-01-17 13:20:48 +0100

    ajout du fichier7

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def islower(c):
-    if len(c) == 1:
-        return c.islower()
-    return False
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False

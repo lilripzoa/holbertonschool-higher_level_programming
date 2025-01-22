3456a7f7b823516c265400e5338cfe536c143717
lilripzoa <soumaryngaka95300@gmail.com>
2025-01-22 11:17:27 +0100

    update de plusieurs fichiers

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    new_string = ""
-    for i in my_string:
-        if i != "c" and i != "C":
-            new_string += i
-    return new_string
+    return ''.join([i for i in my_string if i not in 'cC'])
--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        print(*i)
+    for row in matrix:
+        print(" ".join(["{:d}".format(i) for i in row]))

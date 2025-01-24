12ebeecf666466a8c6b642d3e5998072b53f9c44
lilripzoa <soumaryngaka95300@gmail.com>
2025-01-24 17:06:02 +0100

    update

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def add_integer(a, b=98):
-
+""" This function adds two integers or floats and returns the result."""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):

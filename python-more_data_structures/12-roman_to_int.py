be0d6716042123ff01c9ed979b7e7f4825c1cf49
lilripzoa <soumaryngaka95300@gmail.com>
2025-01-24 11:43:51 +0100

    update

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    if not roman_string or type(roman_string) is not str:
+    if not roman_string:
         return 0
-    roman_dict = {
-        'I': 1, 'V': 5, 'X': 10, 'L': 50,
-        'C': 100, 'D': 500, 'M': 1000
-    }
-    num = 0
+    roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                      'C': 100, 'D': 500, 'M': 1000}
+    int_value = 0
     for i in range(len(roman_string)):
-        if i > 0:
-            prev_value = roman_dict[roman_string[i - 1]]
-            curr_value = roman_dict[roman_string[i]]
-            num += curr_value - 2 * prev_value
-        if roman_dict[roman_string[i]] > roman_dict[roman_string[i - 1]]:
-            prev_value = roman_dict[roman_string[i - 1]]
-            curr_value = roman_dict[roman_string[i]]
-            num += curr_value - 2 * prev_value
+        if i > 0 and roman_numerals[roman_string[i]] > roman_numerals[roman_string[i - 1]]:
+            int_value += roman_numerals[roman_string[i]] - 2 * roman_numerals[roman_string[i - 1]]
         else:
-            num += roman_dict[roman_string[i]]
-    return num
+            int_value += roman_numerals[roman_string[i]]
+    return int_value
+

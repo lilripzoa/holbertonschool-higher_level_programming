c17e7e1e4350f950d866fb58216e971b9187b6b4
lilripzoa <soumaryngaka95300@gmail.com>
2025-01-24 17:07:29 +0100

    update

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def add_integer(a, b=98):
-"""
+    """
     Add two integers
     parameters: a, b (integers) or (floats)
     a is the first number to add
@@ -22,7 +22,7 @@ def add_integer(a, b=98):
     traceback (most recent call last):
         ...
     TypeError: b must be an integer
-"""
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):

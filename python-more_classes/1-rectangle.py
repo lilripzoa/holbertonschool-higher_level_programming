9d8df6b8b1126d97f5b8fe3cf7ae3e86b8a64050
lilripzoa <soumaryngaka95300@gmail.com>
2025-01-29 11:32:25 +0100

    update du 1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 
+""" Rectangle class with width and height attributes"""
+
+
 class Rectangle():
     """a rectangle class"""
 
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+
     @property
     def width(self):
         return self.__width
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -16,9 +21,11 @@ class Rectangle():
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
     @property
     def height(self):
         return self.__height
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
@@ -26,5 +33,3 @@ class Rectangle():
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-
-    

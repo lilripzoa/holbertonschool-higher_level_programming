5fec30fa85898f7c8e267f432464dee8f203b533
lilripzoa <soumaryngaka95300@gmail.com>
2025-02-05 13:11:00 +0100

    fix the issu

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -3,8 +3,7 @@
 """Module that return true if
 object is exactly an instance of the specified class"""
 
-def is_same_class(obj, a_class):
-    """ is_name_class"""
 
-    if type(obj) == a_class:
-        return True
+def is_same_class(obj, a_class):
+    """Return true if object is exactly an instance of the specified class"""
+    return type(obj) == a_class

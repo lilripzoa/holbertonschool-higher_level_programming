2aadb9881ed489277510f2c488d441aace598691
lilripzoa <soumaryngaka95300@gmail.com>
2025-02-05 13:36:28 +0100

    a class that raise a exception with message

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Write a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """a class BaseGeometry"""
+    def area(self):
+        raise Exception("area() is not implemented")

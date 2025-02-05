ec00d8965802fc60bab605ebd0f3552579165801
lilripzoa <soumaryngaka95300@gmail.com>
2025-02-05 13:22:57 +0100

    fix issu

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -5,5 +5,15 @@ object is exactly an instance of the specified class"""
 
 
 def is_same_class(obj, a_class):
-    """Return true if object is exactly an instance of the specified class"""
-    return type(obj) == a_class
+    """
+    Checks if an object is an instance of, or if the
+    object is an instance of
+    a class that inherited from, the specified class.
+    Args:
+        obj: The object to check.
+        a_class: The class to match the type of obj against.
+    Returns:
+        True if obj is an instance or inherited
+        instance of a_class, False otherwise.
+    """
+    return type(obj) is a_class

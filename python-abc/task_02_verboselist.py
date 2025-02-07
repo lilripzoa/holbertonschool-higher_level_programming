8a63d841b0903d68f9dad6e0cb1e6b52dd4a7f43
lilripzoa <soumaryngaka95300@gmail.com>
2025-02-07 14:57:46 +0100

    fix the issues

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -1,33 +1,47 @@
 #!/usr/bin/env python3
 
-"""Verboselist module"""
 
+class VerboseList(list):
+    """
+    A subclass of the built-in list that provides verbose output
+    when performing common list operations.
+    """
 
-def verboselist(list):
-    """Verboselist function"""
-    class Verboselist(list):
-        """Verboselist class"""
-        def append(self, value):
-            """Append method"""
-            print("APPEND {}".format(value))
-            super().append(value)
+    def append(self, item):
+        """
+        Adds an item to the list and prints a message.
+        """
+        super().append(item)
+        print("Added [{}] to the list.".format(item))
 
-        def extend(self, values):
-            """Extend method"""
-            print("Extended the list with {} items.".format(values))
-            super().extend(values)
+    def extend(self, iterable):
+        """
+        Extends the list by appending elements from
+        an iterable and prints a message.
+        """
+        super().extend(iterable)
+        print("Extended the list with [{}] items.".format(len(iterable)))
 
-        def remove(self, value):
-            """Remove method"""
-            print("removed {} from the list.".format(value))
-            super().remove(value)
+    def remove(self, item):
+        """
+        Removes the first occurrence of an item
+        from the list and prints a message.
+        Raises a ValueError if the item is not found.
+        """
+        if item in self:
+            print("Removed [{}] from the list.".format(item))
+            super().remove(item)
+        else:
+            raise ValueError("Item [{}] not found in the list.".format(item))
 
-        def pop(self, index=None):
-            """Pop method"""
-            if index is None:
-                print("Popped {} from the list.".format(self[-1]))
-            else:
-                print("Popped {} from the list.".format(index))
+    def pop(self, index=-1):
+        """
+        Removes and returns the item at the given position in the list.
+        Prints a message and raises a ValueError if the list is empty.
+        """
+        if self:
+            item = self[index]
+            print("Popped [{}] from the list.".format(item))
             return super().pop(index)
-
-    return Verboselist(list)
+        else:
+            raise ValueError("Cannot pop from an empty list.")

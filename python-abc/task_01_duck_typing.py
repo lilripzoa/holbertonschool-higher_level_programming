43f83b52cd58ed3ff5e5d05e9bf7837a7c7bbc9b
lilripzoa <soumaryngaka95300@gmail.com>
2025-02-07 13:38:28 +0100

    fix the issues

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,75 +1,79 @@
 #!/usr/bin/env python3
 
-from typing import Union, List, Tuple, Dict, Any
+from abc import ABC, abstractmethod
+from math import pi
 
-""" Duck typing - first part """
 
+class Shape(ABC):
+    """
+    Abstract base class representing a geometric shape.
+    """
 
-class Shape:
-    """ Shape class """
+    @abstractmethod
     def area(self):
-        raise Exception("area() is not implemented")
+        """
+        Abstract method to calculate the area of the shape.
+        Must be implemented by subclasses.
+        """
+        pass
 
+    @abstractmethod
     def perimeter(self):
-        raise Exception("perimeter() is not implemented")
+        """
+        Abstract method to calculate the perimeter of the shape.
+        Must be implemented by subclasses.
+        """
+        pass
 
 
 class Circle(Shape):
-    """ Circle class """
-    def __init__(self, radius: Union[int, float]):
+    """
+    Class representing a circle, inheriting from Shape.
+    """
+
+    def __init__(self, radius):
         self.__radius = radius
 
     def area(self):
-        """ Area of a circle """
-        return 3.14159265359 * (self.__radius ** 2)
+        """
+        Calculates the area of the circle.
+        """
+        return pi * self.__radius ** 2
 
     def perimeter(self):
-        """ Perimeter of a circle """
-        return 2 * 3.14159265359 * self.__radius
-
-    def __str__(self):
-        """ String representation of a circle """
-        return "[Circle] {}/{}".format(self.__radius, self.__radius)
-
-
-class Square(Shape):
-    """ Square class """
-    def __init__(self, size: Union[int, float]):
-        self.__size = size
-
-    def area(self):
-        """ Area of a square """
-        return self.__size ** 2
-
-    def perimeter(self):
-        """ Perimeter of a square """
-        return 4 * self.__size
-
-    def __str__(self):
-        return "[Square] {}/{}".format(self.__size, self.__size)
-
-
-class shape_info:
-    """ Shape info class """
-    def __init__(self, shape: Shape):
-        print(shape)
-        print("Area:", shape.area())
-        print("Perimeter:", shape.perimeter())
+        """
+        Calculates the perimeter (circumference) of the circle.
+        """
+        return 2 * pi * abs(self.__radius)
 
 
 class Rectangle(Shape):
-    """ Rectangle class """
-    def __init__(self, width: Union[int, float], height: Union[int, float]):
+    """
+    Class representing a rectangle, inheriting from Shape.
+    """
+
+    def __init__(self, width, height):
         self.__width = width
         self.__height = height
 
     def area(self):
-        """ Area of a rectangle """
+        """
+        Calculates the area of the rectangle.
+        """
         return self.__width * self.__height
 
     def perimeter(self):
-        """ Perimeter of a rectangle """
+        """
+        Calculates the perimeter of the rectangle.
+        """
         return 2 * (self.__width + self.__height)
 
-    def __str__(self):
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+def shape_info(shape):
+    """
+    Prints the area and perimeter of a shape
+    """
+    area = shape.area()
+    perimeter = shape.perimeter()
+    print("Area: {}".format(area))
+    print("Perimeter: {}".format(perimeter))

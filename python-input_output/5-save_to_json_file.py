56975f9833c2e6bede6342e75c6c7a698fb250f9
lilripzoa <soumaryngaka95300@gmail.com>
2025-02-12 12:00:03 +0100

    fix issues

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -11,5 +11,5 @@ def save_to_json_file(my_obj, filename):
     returns:
         None
     """
-    with open(filename, "w", encoding="utf-8") as f:
-        json.dump(my_obj, f)
+    with open(filename, 'w', encoding="utf-8") as fic:
+        json.dump(my_obj, fic)

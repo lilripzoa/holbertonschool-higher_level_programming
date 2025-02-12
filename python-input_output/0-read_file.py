f2645e5c282d6c24b4c62781cec8bbcc8878e6ac
lilripzoa <soumaryngaka95300@gmail.com>
2025-02-12 11:37:19 +0100

    function that reads a text file (UTF8) and prints it to stdout:

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Read a file and print its contents
+to the standard output."""
+
+def read_file(filename=""):
+    """
+    Read a file and print its contents
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")

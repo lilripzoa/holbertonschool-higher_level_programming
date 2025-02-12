f9cfe953fd6b2deb9cc8cff3a7539fbba70cf602
lilripzoa <soumaryngaka95300@gmail.com>
2025-02-12 12:22:31 +0100

    fix issues

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,10 +7,5 @@ import sys
 def add_item():
     """Add all arguments to a Python list,
     and then save them to a file"""
-    try:
-        my_list = load_from_json_file("add_item.json")
-    except:
-        my_list = []
-    for i in range(1, len(sys.argv)):
-        my_list.append(sys.argv[i])
-    save_to_json_file(my_list, "add_item.json")
+    with open(filename, 'r', encoding="utf-8") as fic:
+        return json.load(fic)

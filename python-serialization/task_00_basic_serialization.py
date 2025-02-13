33c347d310eaab755983ace3e5f4655eed79516e
lilripzoa <soumaryngaka95300@gmail.com>
2025-02-13 20:29:33 +0100

    fix issues

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -10,12 +10,13 @@ def serialize_and_save_to_file(data, filename):
     data: data to serialize
     filename: file to save data
     """
-    with open(filename, 'w') as f:
-        f.write(data)
+    with open(filename, 'w', encoding='utf-8') as fic:
+        json.dump(data, fic)
+
 
 def load_and_deserialize(filename):
     """ Deserializes data from a file
     filename: file to load data
     """
-    with open(filename, 'r') as f:
-        return json.load(f)
+    with open(filename, 'r', encoding='utf-8') as fic:
+        return json.load(fic)

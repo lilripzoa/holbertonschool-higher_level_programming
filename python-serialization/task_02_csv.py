a86e0c166c7be857ace8527ac0507c3a61e90d89
lilripzoa <soumaryngaka95300@gmail.com>
2025-02-13 20:55:24 +0100

    fix issues

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -4,15 +4,17 @@
 import csv
 import json
 
-python import csv import json
-
 
 def convert_csv_to_json(csv_file):
-    """ Convert CSV to JSON
-    """
-    with open(csv_file, mode='r') as file:
-        csv_reader = csv.DictReader(file)
-        data = [row for row in csv_reader]
-    with open('data.json', mode='w') as file:
-        json.dump(data, file)
-    return data
+    try: 
+        data = []
+        with open(csv_file, 'r') as fric:
+            csv_reader = csv.DictReader(fric)
+            for row in csv_reader:
+                data.append(row)
+
+        with open("data.json", 'w') as fric:
+            json.dump(data, fric, indent= 4)
+            return True
+    except FileNotFoundError:
+        return False

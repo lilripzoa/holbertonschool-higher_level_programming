9303f647aa2c9196b6725e4b25c27b8132e0ee9d
lilripzoa <soumaryngaka95300@gmail.com>
2025-02-13 20:10:58 +0100

    fix issues

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,22 +2,28 @@
 """
 Loads an object from a text file containing JSON data.
 """
-
-
-import sys
+import json
 import os.path
+import sys
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
 
 args = sys.argv[1:]
 
 filename = "add_item.json"
 
-try:
-    my_list = load_from_json_file(filename)
-
-except FileNotFoundError:
+if os.path.exists(filename):
+    try:
+        my_list = load_from_json_file(filename)
+    except json.JSONDecodeError:
+        my_list = []
+else:
     my_list = []
 
+
 my_list.extend(args)
+
+
 save_to_json_file(my_list, filename)

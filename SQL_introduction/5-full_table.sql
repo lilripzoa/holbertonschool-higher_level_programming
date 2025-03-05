68ceef86d5c7ffb46018a1ea92759c74f972d0d3
lilripzoa <soumaryngaka95300@gmail.com>
2025-03-05 14:40:54 +0100

    fix issu

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
 -- print the description of the table first_table from the database
-SELECT *
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA ='hbtn_0c_0' AND TABLE_NAME = 'first_table';
+DESC first_table;

24a7932b2eeac2e70c6e87bc65e4c5c21a4dd667
lilripzoa <soumaryngaka95300@gmail.com>
2025-03-06 20:55:35 +0100

    script that lists all the cities of California that can be found in the database hbtn_0d_usa.

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,5 +1,5 @@
 -- script that creates the table force_name on my MySQL server.
-CREATE TABLE IF NOT EXISTS force_name (
+CREATE TABLE IF NOT EXISTS cat 8-cities_of_california_subquery.sql | mysql -hlocalhost -uroot -p hbtn_0d_usaforce_name (
     id INT,
     name VARCHAR(256)
 );
---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,3 @@
+--  script that lists all the cities of California that can be found in the database hbtn_0d_usa.
+SELECT cities FROM states;
+ORDER BY cities.id ASC;

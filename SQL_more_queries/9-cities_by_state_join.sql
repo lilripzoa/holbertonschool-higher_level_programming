f4a38271a66103b1f9c3b83fd0099988a72943d0
lilripzoa <soumaryngaka95300@gmail.com>
2025-03-07 12:27:54 +0100

    fix issu

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,4 @@
 -- script that lists all cities contained in the database hbtn_0d_usa.
-SELECT id, name FROM cities;
-SELECT name FROM states;
+SELECT id, name 
+FROM cities
+WHERE state_id = (SELECT id FROM states WHERE name = 'California');
